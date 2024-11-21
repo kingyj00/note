3020888fa6cc709e4cd41f7726c4ca2db9f057ba
kingyj00 <onshopyj@gmail.com>
2024-11-21 11:31:36 +0900

    abstrat_game_2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -11,6 +11,7 @@ public class Main {
         // 출력 : 전사가 칼로 공격합니다.
 
         a전사.이름 = "초코";
+        a전사.나이 = 23;
         a전사.a무기 = new 활();
         a전사.공격();
         // 출력 : 전사가 활로 공격합니다.
@@ -18,7 +19,7 @@ public class Main {
 }
 
 abstract class 무기 {
-    abstract void 작동();
+    abstract void 작동(String 사용자_이름, int 사용자_나이);
 }
 
 class 전사 {
@@ -26,18 +27,19 @@ class 전사 {
     int 나이;
     무기 a무기;
             void 공격() {
-        a무기.작동();
+        a무기.작동(이름, 나이);
     }
 }
 
 class 칼 extends 무기 {
-    void 작동() {
-        System.out.println("전사가 칼로 공격합니다.");
+    void 작동(String 사용자_이름, int 사용자_나이) {
+        System.out.println(사용자_나이 + "(살) 전사" + 사용자_이름 + "(이)가 칼(으)로 공격합니다.");
+
     }
 }
 
 class 활 extends 무기 {
-    void 작동() {
-        System.out.println("전사가 활로 공격합니다.");
+    void 작동(String 사용자_이름, int 사용자_나이) {
+        System.out.println(사용자_나이 + "(살) 전사" + 사용자_이름 + "(이)가 활(으)로 공격합니다.");
     }
 }
