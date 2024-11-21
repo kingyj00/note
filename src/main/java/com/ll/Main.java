2857a93e406cdd3816e8438c4b294c4b9dd13f7e
kingyj00 <onshopyj@gmail.com>
2024-11-21 11:23:15 +0900

    abstrat_game_1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,79 +2,42 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        표준Tv a표준Tv;
-        샤오미Tv a샤오미Tv = new 샤오미Tv();
-        삼성Tv a삼성Tv = new 삼성Tv();
-        LGTv aLGTv = new LGTv();
+        전사 a전사 = new 전사();
 
-        a표준Tv = a샤오미Tv;
-        a표준Tv.켜기();
-        // 출력 : 샤오미TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 샤오미TV 꺼집니다.
+        a전사.이름 = "카니";
+        a전사.나이 = 22;
+        a전사.a무기 = new 칼();
+        a전사.공격();
+        // 출력 : 전사가 칼로 공격합니다.
 
-        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
-        a표준Tv = a삼성Tv;
-        a표준Tv.켜기();
-        // 출력 : 삼성TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 삼성TV 꺼집니다.
-
-        // a표준Tv 변수에 LGTv 객체를 연결한다.
-        a표준Tv = aLGTv;
-        a표준Tv.켜기();
-        // 출력 : LGTV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : LGTV 꺼집니다.
-
-        LGTv aLGTv2 = (LGTv) a표준Tv;
-        aLGTv2.게임모드전환();
-    }
-}
-abstract class 표준Tv {
-    abstract void 켜기();
-
-    abstract void 끄기();
-}
-
-class 샤오미Tv extends 표준Tv {
-    void 켜기() {
-        System.out.println("샤오미Tv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("샤오미Tv 꺼집니다.");
-    }
-
-    void vr켜기() {
-        System.out.println("샤오미Tv vr켜기!");
+        a전사.이름 = "초코";
+        a전사.a무기 = new 활();
+        a전사.공격();
+        // 출력 : 전사가 활로 공격합니다.
     }
 }
 
-class 삼성Tv extends 표준Tv {
-    void 켜기() {
-        System.out.println("삼성Tv 켜집니다.");
-    }
+abstract class 무기 {
+    abstract void 작동();
+}
 
-    void 끄기() {
-        System.out.println("삼성Tv 꺼집니다.");
-    }
-
-    void ar켜기() {
-        System.out.println("삼성Tv ar켜기!");
+class 전사 {
+    String 이름;
+    int 나이;
+    무기 a무기;
+            void 공격() {
+        a무기.작동();
     }
 }
 
-class LGTv extends 표준Tv {
-    void 켜기() {
-        System.out.println("LGTv 켜집니다.");
+class 칼 extends 무기 {
+    void 작동() {
+        System.out.println("전사가 칼로 공격합니다.");
     }
+}
 
-    void 끄기() {
-        System.out.println("LGTv 꺼집니다.");
-    }
-
-    void 게임모드전환() {
-        System.out.println("LGTv 게임모드전환!");
+class 활 extends 무기 {
+    void 작동() {
+        System.out.println("전사가 활로 공격합니다.");
     }
 }
