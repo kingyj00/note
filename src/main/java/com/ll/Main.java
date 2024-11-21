9cae9f5a2456e2986dd9b2e94e5c65dc11d796cb
kingyj00 <onshopyj@gmail.com>
2024-11-21 09:44:15 +0900

    void1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,12 +2,63 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        int age = 5;
+        샤오미Tv a샤오미Tv = new 샤오미Tv();
+        삼성Tv a삼성Tv = new 삼성Tv();
+        LGTv aLGTv = new LGTv();
 
-// v1
-        if ( age < 10 ) { System.out.println("아동/영유아"); }
-        else if ( age < 20 ) { System.out.println("10대"); }
-        else { System.out.println("20대 이상"); } // 전체(3가지) 경우의 수 중에서 무조건 정답이 1개 존재한다면, 마지막 if문의 조건식은 지우는게 좋다.
+        a샤오미Tv.켜기();
+        // 출력 => 샤오미Tv 켜집니다.
+        a샤오미Tv.끄기();
+        // 출력 => 샤오미Tv 꺼집니다.
+        a샤오미Tv.vr켜기();
+        // 출력 => 샤오미Tv vr켜기!
 
+        a삼성Tv.켜기();
+        // 출력 => 삼성Tv 켜집니다.
+        a삼성Tv.끄기();
+        // 출력 => 삼성Tv 꺼집니다.
+        a삼성Tv.ar켜기();
+        // 출력 => 삼성Tv ar켜기!
+
+        aLGTv.켜기();
+        // 출력 => LGTv 켜집니다.
+        aLGTv.끄기();
+        // 출력 => LGTv 꺼집니다.
+        aLGTv.게임모드전환();
+        // 출력 => LGTv 게임모드전환!
+    }
+}
+
+class 삼성Tv {
+    void 켜기() {
+        System.out.println("삼성Tv 켜집니다.");
+    }
+    void 끄기() {
+        System.out.println("삼성Tv 꺼집니다.");
+    }
+    void ar켜기() {
+        System.out.println("삼성Tv ar켜기!");
+    }
+}
+class LGTv {
+    void 켜기() {
+        System.out.println("LGTv 켜집니다.");
+    }
+    void 끄기() {
+        System.out.println("LGTv 꺼집니다.");
+    }
+    void 게임모드전환() {
+        System.out.println("LGTv 게임모드전환!");
+    }
+}
+class 샤오미Tv {
+    void 켜기() {
+        System.out.println("샤오미Tv 켜집니다.");
+    }
+    void 끄기() {
+        System.out.println("샤오미Tv 꺼집니다.");
+    }
+    void vr켜기() {
+        System.out.println("샤오미Tv vr켜기!");
     }
 }
