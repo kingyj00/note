f2fa98f85144659963e0844139d2f992c4f87d3d
kingyj00 <onshopyj@gmail.com>
2024-11-25 15:42:03 +0900

    NumberList-3

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,75 +1,38 @@
 package com.ll;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
-        List<Person> people = new ArrayList<>();
-        people.add(new Person(1, "Alice", 20, 'F'));
-        people.add(new Person(2, "Bob", 25, 'M'));
-        people.add(new Person(3, "David", 35, 'M'));
-
-        // 문제 : 남성의 이름들
-
-        System.out.println("== No Stream ==");
-        noStreamVersion(people);
-
-        System.out.println("== Stream ==");
-        streamVersion(people);
+        전화번호 app = new 전화번호();
+        app.run ();
     }
+}
 
-    private static void noStreamVersion(List<Person> people) {
-        List<String> names = new ArrayList<>();
+class 전화번호 {
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("■■■■■■[ 번호 목록 ]■■■■■■");
+        System.out.println("번호 등록을 원하면 [등록]");
+        System.out.println("종료를 원하면 [종료]");
+        System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■");
 
-        for (Person person : people) {
-            if (person.getGender() == 'M') {
-                names.add(person.getName());
+
+        while (true) {
+            System.out.print("입력 : ");
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("이름 : ");
+                String name = scanner.nextLine();
+                System.out.print("전화번호 : ");
+                String number = scanner.nextLine();
+
             }
         }
-
-        String manNames = String.join(", ", names);
-
-        System.out.println("manNames : " + manNames);
-    }
-
-    private static void streamVersion(List<Person> people) {
-        String manNames = people.stream()
-                .filter(person -> person.getGender() == 'M')
-                .map(Person::getName)
-                .collect(Collectors.joining(", "));
-
-        System.out.println("manNames : " + manNames);
+        scanner.close();
     }
 }
 
-class Person {
-    private int id;
-    private String name;
-    private int age;
-    private char gender;
-
-    public Person(int id, String name, int age, char gender) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public char getGender() {
-        return gender;
-    }
-}
