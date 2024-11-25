d54394cd0d28c0a7b67225d8fd51688ac8c0daef
kingyj00 <onshopyj@gmail.com>
2024-11-25 12:39:04 +0900

    Scream-sum

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,20 +1,73 @@
 package com.ll;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        // 문자열 배열
-        String[] input = {"1번", "2번", "3번"};
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(1, "Alice", 20, 'F'));
+        people.add(new Person(2, "Bob", 25, 'M'));
+        people.add(new Person(3, "David", 35, 'M'));
 
-        // 짝수 제거하고 정수 배열로 변환
-        int[] result = Arrays.stream(input) // 문자열 배열을 스트림으로 변환
-                .map(s -> Integer.parseInt(s.replaceAll("[^0-9]", ""))) // 숫자만 추출
-                .filter(num -> num % 2 != 0) // 짝수 제거
-                .mapToInt(num -> num) // int 스트림으로 변환
-                .toArray(); // 결과를 int 배열로 변환
+        // 문제 : 남성들의 나이의 합
 
-        // 결과 출력
-        System.out.println(Arrays.toString(result));
+        System.out.println("== No Stream ==");
+        noStreamVersion(people);
+
+        System.out.println("== Stream ==");
+        streamVersion(people);
+    }
+
+    private static void noStreamVersion(List<Person> people) {
+        int sum = 0;
+
+        for (Person person : people) {
+            if (person.getGender() == 'M') {
+                sum += person.getAge();
+            }
+        }
+
+        System.out.println("sum of age : " + sum);
+    }
+
+    private static void streamVersion(List<Person> people) {
+        int sum = people
+                .stream()
+                .filter(e -> e.getGender() == 'M')
+                .mapToInt(e -> e.getAge())
+                .sum();
+
+        System.out.println("sum of age : " + sum);
+    }
+}
+
+class Person {
+    private int id;
+    private String name;
+    private int age;
+    private char gender;
+
+    public Person(int id, String name, int age, char gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getGender() {
+        return gender;
     }
 }
