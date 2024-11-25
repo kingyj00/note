79958ddee0cc003064ca142220789ff7ddeff873
kingyj00 <onshopyj@gmail.com>
2024-11-25 10:05:11 +0900

    NumberList-2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -11,9 +11,17 @@ public class Main {
 
 class 전화번호 {
     public void run(){
-        System.out.println("번호 목록");
-        System.out.println("명령 : ");
-
         Scanner scanner = new Scanner(System.in);
+        System.out.println("번호 목록");
+
+        while (true) {
+            System.out.println("명령 : ");
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료")) break;
+
+            System.out.println("입력값 : " + cmd);
+        }
+        scanner.close();
     }
 }
