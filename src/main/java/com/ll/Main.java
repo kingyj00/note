90d4f746ed2d917f83bb624c2d9a37981ff10955
kingyj00 <onshopyj@gmail.com>
2024-11-27 21:57:00 +0900

    Stream-Class5

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,34 +2,52 @@ package com.ll;
 
 import java.util.Arrays;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        System.out.println("===== NO Stream =====");
-        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("== No Stream ==");
+        noStreamVersion();
 
-        System.out.println("===== 기존 Numbers =====");
-        System.out.println(Arrays.toString(numbers));
+        System.out.println("== Stream v1 ==");
+        streamVersionV1();
 
-        for (int i=0; i < numbers.length; i++){
-            numbers[i] *= 2;
+        System.out.println("== Stream v2 ==");
+        streamVersionV2();
+    }
+
+    private static void noStreamVersion() {
+        int[] numbers = new int[]{1, 2, 3};
+
+        String[] messages = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) continue;
+
+            messages[i] = numbers[i] + "번";
         }
-        System.out.println("===== 변환된 Numbers =====");
+
+        System.out.println("= 원본 =");
         System.out.println(Arrays.toString(numbers));
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(messages));
+    }
 
-        System.out.println("===== Stream =====");
-
-        System.out.println("===== Stream(x) Numbers =====");
-        numbers = new int[] {1,2,3,4,5,6,7,8,9,10};
-
-        int[] calculatedNumbers = Arrays.stream(numbers)
-                .map(e -> e * 2)
+    private static void streamVersionV1() {
+        Object[] array = Arrays.stream(new int[]{1, 2, 3})
+                .filter(e -> e % 2 != 0)
+                .mapToObj(e -> e + "번")
                 .toArray();
 
-        System.out.println("===== Stream(o) Numbers =====");
-        System.out.println(Arrays.toString(numbers));
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(array));
+    }
 
-        System.out.println("===== Stream 새 배열 calculatedNumbers =====");
-        System.out.println(Arrays.toString(numbers));
+    private static void streamVersionV2() {
+        String[] array = Arrays.stream(new int[]{1, 2, 3})
+                .filter(e -> e % 2 != 0)
+                .mapToObj(e -> e + "번")
+                .toArray(String[]::new);
+
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(array));
     }
 }
-
