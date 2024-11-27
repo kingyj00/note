dc113faf1d9aa7f17707461fbee11b9a0958774a
kingyj00 <onshopyj@gmail.com>
2024-11-27 21:46:29 +0900

    Stream-Class4

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,14 +1,35 @@
 package com.ll;
 
-// 개인 참조 : 55강(완)
-// 참고 : 명언게시판 8단계 / 55강
-// 단계 : 명언 수정 get / set
-// 전단계 > ArrayList 사용해서 삭제처리와 없는 것 예외처리
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        전화번호 app = new 전화번호();
-        app.run ();
+        System.out.println("===== NO Stream =====");
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println("===== 기존 Numbers =====");
+        System.out.println(Arrays.toString(numbers));
+
+        for (int i=0; i < numbers.length; i++){
+            numbers[i] *= 2;
+        }
+        System.out.println("===== 변환된 Numbers =====");
+        System.out.println(Arrays.toString(numbers));
+
+        System.out.println("===== Stream =====");
+
+        System.out.println("===== Stream(x) Numbers =====");
+        numbers = new int[] {1,2,3,4,5,6,7,8,9,10};
+
+        int[] calculatedNumbers = Arrays.stream(numbers)
+                .map(e -> e * 2)
+                .toArray();
+
+        System.out.println("===== Stream(o) Numbers =====");
+        System.out.println(Arrays.toString(numbers));
+
+        System.out.println("===== Stream 새 배열 calculatedNumbers =====");
+        System.out.println(Arrays.toString(numbers));
     }
 }
 
