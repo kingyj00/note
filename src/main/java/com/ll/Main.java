190c0604801e16a003260c5e394a3efd7057a9ba
kingyj00 <onshopyj@gmail.com>
2025-01-03 12:59:46 +0900

    Stream-3

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -6,21 +6,17 @@ class Main {
     public static void main(String[] args) {
         // 일반
         for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) continue;
+
             System.out.println(i);
         }
 
-        // 스트림
+        // 스트림, 인텔리제이에서 브레이크 포인트 걸어서 Trace Current Stream Chain 뷰를 확인해보세요.
         IntStream.rangeClosed(1, 10)
+                .filter(e -> e % 2 != 0)
                 .forEach(e -> {
                     System.out.println(e);
                 });
 
-        // 스트림
-        IntStream.rangeClosed(1, 10)
-                .forEach(e -> System.out.println(e));
-
-        // 스트림
-        IntStream.rangeClosed(1, 10)
-                .forEach(System.out::println);
     }
 }
