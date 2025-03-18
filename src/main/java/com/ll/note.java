34f1f18356e5858b741b4b6a23ecd0245e1afca1
kingyj00 <onshopyj@gmail.com>
2025-03-18 14:58:41 +0900

    java 복습_01

--- src/main/java/com/ll/note.java
+++ src/main/java/com/ll/note.java
@@ -1,65 +1,7 @@
 package com.ll;
 
-import java.util.*;
-
-class Solution {
-    public int solution(String[] board) {
-        int n = board.length;
-        int m = board[0].length();
-        int[] start = new int[2];
-        int[] goal = new int[2];
-
-        // 시작 위치와 목표 위치 찾기
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i].charAt(j) == 'R') {
-                    start[0] = i;
-                    start[1] = j;
-                } else if (board[i].charAt(j) == 'G') {
-                    goal[0] = i;
-                    goal[1] = j;
-                }
-            }
-        }
-
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{start[0], start[1], 0});
-        boolean[][] visited = new boolean[n][m];
-        visited[start[0]][start[1]] = true;
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
-            int steps = current[2];
-
-            if (x == goal[0] && y == goal[1]) {
-                return steps;
-            }
-
-            for (int[] dir : directions) {
-                int nx = x;
-                int ny = y;
-
-                while (true) {
-                    nx += dir[0];
-                    ny += dir[1];
-
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx].charAt(ny) == 'D') {
-                        nx -= dir[0];
-                        ny -= dir[1];
-                        break;
-                    }
-                }
-
-                if (!visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    queue.offer(new int[]{nx, ny, steps + 1});
-                }
-            }
-        }
-
-        return -1;
+public class note {
+    public static void main(String[] args) {
+        System.out.println("안녕");
     }
 }
