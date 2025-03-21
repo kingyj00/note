c4697966bde6a8ac5297c7cd3898669d49de0b0b
kingyj00 <onshopyj@gmail.com>
2025-03-21 18:20:16 +0900

    java 복습_02

--- src/main/java/com/ll/note.java
+++ src/main/java/com/ll/note.java
@@ -1,7 +1,25 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class note {
     public static void main(String[] args) {
-        System.out.println("안녕");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("입력: ");
+            String input = scanner.nextLine();
+
+            if (input.equals("생성")) {
+                System.out.println("새로운 입력란 생성!");
+            } else if (input.equals("종료")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else {
+                System.out.println("입력값: " + input);
+            }
+        }
+
+        scanner.close();
     }
 }
